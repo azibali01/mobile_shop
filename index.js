99d348f8c372511574661164d22da368cddc74c1
azibali01 <azibaliansari311@gmail.com>
2025-12-25 20:46:27 +0500

    test 3

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const customerRoutes = require('./routes/customerRoutes');
+const authRoutes = require('./routes/authRoutes'); 
 
 dotenv.config();
 const app = express();
 
-
-
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json()); 
 app.use('/api/customers', customerRoutes);
+
+
+app.use('/api/auth', authRoutes); 
 
 
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log(" MongoDB Connected Successfully!"))
-
-    .catch((err) => console.log("DB Connection Error:", err));
+    .then(() => console.log("MongoDB Connected Successfully!"))
+    .catch((err) => console.log(" DB Connection Error:", err));
     
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-
-    console.log(`Server running on: http://localhost:${PORT}`);
+    console.log(` Server running on: http://localhost:${PORT}`);
 });
