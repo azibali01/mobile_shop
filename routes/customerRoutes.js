3391bb0df29b6ea9355b04224f0e5f16e7b19423
azibali01 <azibaliansari311@gmail.com>
2025-12-25 19:20:51 +0500

    search added

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,43 +1,57 @@
+
+
 const express = require('express');
 const router = express.Router();
-const Customer =  require('../models/customer');
-
-
-
-
+const Customer = require('../models/customer');
 
 router.get('/', async (req, res) => {
-
     try {
+    
+        const { cnic, emi } = req.query;
+        
+        
+        let filter = {};
 
-        const customers = await Customer.find().sort({ createdAt: -1 });
+        
+        if (cnic) {
+            filter.cnic = cnic; 
+
+
+        }
+
+        if (emi) {
+            filter.emi = emi; 
+        }
+
+        const customers = await Customer.find(filter).sort({ createdAt: -1 });
+
+    
+
+        if (customers.length === 0) {
+            return res.status(404).json({ message: "no result found" });
+        }
+
         res.json(customers);
 
     } catch (err) {
-
         res.status(500).json({ message: err.message });
     }
 });
 
 
-router.post('/', async (req, res) => {
 
+
+router.post('/', async (req, res) => {
     const customer = new Customer(req.body);
     try {
 
+
         const newCustomer = await customer.save();
-
         res.status(201).json(newCustomer);
-
     } catch (err) {
-
         res.status(400).json({ message: err.message });
-
     }
 });
-
-
-
 
 
 
@@ -46,11 +60,8 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
 
     try {
-
         const updatedCustomer = await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true });
-
         res.json(updatedCustomer);
-
     } catch (err) {
 
         res.status(400).json({ message: err.message });
@@ -58,18 +69,14 @@ router.put('/:id', async (req, res) => {
 });
 
 
+
+
 router.delete('/:id', async (req, res) => {
-
     try {
-
         await Customer.findByIdAndDelete(req.params.id);
-
         res.json({ message: 'Customer Deleted' });
-
     } catch (err) {
-
         res.status(500).json({ message: err.message });
-        
     }
 });
 
